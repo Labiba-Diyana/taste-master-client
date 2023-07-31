1f4f4a41ea617702e4507ece283626ac5c0c2837
diyana <labibadiyana100@gmail.com>
2023-07-31 22:23:18 +0600

    recipe page done

--- src/pages/ViewRecipes/Recipe.jsx
+++ src/pages/ViewRecipes/Recipe.jsx
@@ -1,10 +1,23 @@
 
 
-const Recipe = ({recipe}) => {
-    console.log(recipe)
+const Recipe = ({ recipe }) => {
+    const { recipeName, ingredients, method, ratings } = recipe;
     return (
-        <div>
-            
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body italic space-y-2">
+                <h2 className="text-2xl font-extrabold">{recipeName}</h2>
+                <p>Ingredients: {ingredients}</p>
+                <hr />
+                <p className="h-[170px] overflow-hidden hover:overflow-auto">Method: {method}</p>
+                <hr />
+                <div className="flex justify-between items-center">
+                    <p>Ratings: {ratings}</p>
+                    <button className="btn bg-white border-none text-[#DAA425]">
+                        Favourite
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/ViewRecipes/ViewRecipes.jsx
+++ src/pages/ViewRecipes/ViewRecipes.jsx
@@ -7,8 +7,8 @@ const ViewRecipes = () => {
     const { _id, picture, name, bio, experience, recipes, likes, recipeInfo } = data
 
     return (
-        <div className="pt-28 pb-32">
-            <div className="flex items-center justify-between space-x-14 pt-16 w-9/12 mx-auto">
+        <div className="pt-28 pb-32 w-10/12 mx-auto">
+            <div className="flex items-center justify-between space-x-14 pt-16 ">
                 <div className="avatar">
                     <div className="w-56 h-56 rounded-full">
                         <img src={picture} />
@@ -22,7 +22,7 @@ const ViewRecipes = () => {
                     <p>Likes: {likes}</p>
                 </div>
             </div>
-            <div>
+            <div className="mt-20 grid grid-cols-3">
                 {
                     recipeInfo.map(recipe => <Recipe
                         key={recipe.id}
