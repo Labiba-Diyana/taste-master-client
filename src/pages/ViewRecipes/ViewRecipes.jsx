c23eec6dec6028875227126ce3a48d42cbca4f9f
diyana <labibadiyana100@gmail.com>
2023-08-03 23:44:00 +0600

    react-lazy-load component added to chef profile

--- src/pages/ViewRecipes/ViewRecipes.jsx
+++ src/pages/ViewRecipes/ViewRecipes.jsx
@@ -1,17 +1,22 @@
 import { useLoaderData } from "react-router-dom";
 import Recipe from "./Recipe";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
 
 
 const ViewRecipes = () => {
     const data = useLoaderData();
-    const { _id, picture, name, bio, experience, recipes, likes, recipeInfo } = data
+    const { picture, name, bio, experience, recipes, likes, recipeInfo } = data
 
     return (
         <div className="pt-28 pb-32 w-10/12 mx-auto">
             <div className="flex items-center justify-between space-x-14 pt-16 ">
                 <div className="avatar">
                     <div className="w-56 h-56 rounded-full">
-                        <img src={picture} />
+                        <LazyLoadImage className="w-full h-full rounded-full"
+                            effect="blur"
+                            src={picture} />
                     </div>
                 </div>
                 <div className="italic font-normal text-xl space-y-7 pl-7 border-l-4 border-[#DAA425]">
